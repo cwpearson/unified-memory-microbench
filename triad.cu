6160979db18b0f76a7e714b7b212e3287804292e
Carl Pearson <pearson@illinois.edu>
2019-06-04 06:59:27 -0500

    add more command line flags

--- triad.cu
+++ triad.cu
@@ -20,11 +20,11 @@ void print_header(size_t numIters) {
 
 void print(const std::string &name, const size_t bytes, const Results &results) {
   // print times
-  printf("%s\t%lu\t%s", name.c_str(), bytes, "s");
-  for (auto t : results.times) {
-    printf("\t%.1e", t);
-  }
-  printf("\n");
+  //printf("%s\t%lu\t%s", name.c_str(), bytes, "s");
+  //for (auto t : results.times) {
+  //  printf("\t%.1e", t);
+  //}
+  //printf("\n");
 
   // print metrics
   printf("%s\t%lu\t%s", name.c_str(), bytes, results.unit.c_str());
@@ -45,20 +45,35 @@ int main(int argc, char **argv) {
   ("b,bytes",
    "Number of bytes",
     cxxopts::value<size_t>()->default_value("2147483648")) // 2G
+  ("m,megabytes",
+   "Number of megabytes",
+    cxxopts::value<double>()->default_value("2048.0")) // 2G
+  ("g,gigabytes",
+   "Number of gigabytes",
+    cxxopts::value<double>()->default_value("2.0")) // 2G
   ("h,help",
    "Show help")
   ;
   auto result = options.parse(argc, argv);
 
   bool help = result["help"].as<bool>();
-
   if (help) {
     printf("%s\n", options.help().c_str());
     exit(0);
   }
 
+  size_t bytes = 0;
+  if (result["gigabytes"].count()) {
+    bytes = result["gigabytes"].as<double>() * (1024ul * 1024ul * 1024ul);
+  }
+  else if (result["megabytes"].count()) {
+    bytes = result["megabytes"].as<double>() * (1024ul * 1024ul);
+  } 
+  else {
+    bytes = result["bytes"].as<size_t>();
+  }
+
   size_t numIters = result["num-iters"].as<size_t>();
-  size_t bytes = result["bytes"].as<size_t>();
 
   print_header(numIters);
 
